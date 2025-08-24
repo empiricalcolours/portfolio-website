6ef9a73c933eb7008f804aeb39fa91db36d4e291
Gaurav Warad <waradgaurav@yahoo.in>
2025-08-24 14:49:42 -0700

    font changes for artshowcase

--- empirical-colours/src/components/ArtShowcase.tsx
+++ empirical-colours/src/components/ArtShowcase.tsx
@@ -89,8 +89,8 @@ const ArtShowcase: React.FC = () => {
           <ImageListItem key={item.src} onClick={() => handleOpen(index)} sx={{ cursor: 'pointer' }}>
             <img src={item.src} alt={item.title} loading="lazy" style={{ width: '100%', borderRadius: 8 }} />
             <ImageListItemBar
-              title={item.title}
-              subtitle={item.description}
+              title={<Typography sx={{ fontFamily: 'Comic Neue, cursive', fontWeight: 400 }}>{item.title}</Typography>}
+              subtitle={<Typography sx={{ fontFamily: 'Comic Neue, cursive', fontWeight: 300 }}>{item.description}</Typography>}
               position="below"
               sx={{ textAlign: 'center' }}
             />
@@ -133,8 +133,8 @@ const ArtShowcase: React.FC = () => {
             </Box>
 
             <Box mt={2} textAlign="center">
-              <Typography variant="h6">{currentImages[selectedIndex].title}</Typography>
-              <Typography variant="body2">{currentImages[selectedIndex].description}</Typography>
+              <Typography variant="h6" sx={{ fontFamily: 'Comic Neue, cursive', fontWeight: 400 }}>{currentImages[selectedIndex].title}</Typography>
+              <Typography variant="body2" sx={{ fontFamily: 'Comic Neue, cursive', fontWeight: 300 }}>{currentImages[selectedIndex].description}</Typography>
             </Box>
           </DialogContent>
         )}
